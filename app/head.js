61dd67598ea6b8897cd7599e7422627deaa76917
3illBaby <thrillanon@outlook.com>
2023-07-11 18:10:06 +0100

    added app dir

---
+++ app/head.js
@@ -0,0 +1,9 @@
+const Head = () => (
+  <>
+    <title>CYB3RVERSE</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="thrill.png" />
+  </>
+);
+
+export default Head;
